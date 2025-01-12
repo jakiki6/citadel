0e3668993771d803444086634e4b6920c92e32df
Jakob Kirsch <jakob.kirsch@web.de>
2025-01-12 08:42:30 +0100

    added test input

--- bootrom/main.c
+++ bootrom/main.c
@@ -1,6 +1,6 @@
-static char *tx = (char *) 0x01000004;
-static char *rx = (char *) 0x01000004;
-static char *rx_ready = (char *) 0x01000008;
+static volatile char *tx = (char *) 0x01000004;
+static volatile char *rx = (char *) 0x01000004;
+static volatile char *rx_ready = (char *) 0x01000008;
 
 void main() {
     char *msg = "Hello world!\n";
@@ -8,4 +8,6 @@ void main() {
         *tx = *msg;
         msg++;
     }
+
+    while (!(*rx_ready));
 }
