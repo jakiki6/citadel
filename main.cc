4712022306fbb46b9a6ebd90870bd5666ef13218
Jakob Kirsch <jakob.kirsch@web.de>
2025-01-12 08:41:13 +0100

    added input

--- main.cc
+++ main.cc
@@ -1,3 +1,5 @@
+#include <fcntl.h>
+
 #include "Vcitadel.h"
 #include "verilated.h"
 
@@ -18,6 +20,7 @@ int main(int argc, char **argv) {
 
     top->rst_n = 1;
 
+    fcntl(fileno(stdin), F_SETFL, fcntl(fileno(stdin), F_GETFL) | O_NONBLOCK);
     while (!top->panic) {
         top->r_clk = !top->r_clk;
         top->eval();
@@ -25,6 +28,18 @@ int main(int argc, char **argv) {
         if (top->tx_ready && top->r_clk == 0) {
             putc(top->tx, stdout);
             fflush(stdout);
+        }
+
+        if (top->rx_ack) {
+            top->rx_ready = 0;
+        }
+
+        if (!top->rx_ready) {
+            int c = getc(stdin);
+            if (c >= 0) {
+                top->rx = c & 0xff;
+                top->rx_ready = 1;
+            }
         }
     }
 
